0f1f8e6a88d5f48195b4c0cec4b455d105227609
Bobby Ranjan <bbbranjan@gmail.com>
2019-10-10 12:18:02 +0800

    Sped up kNN smoothing using sparse adjacency matrix.

--- R/kNNSmoothing.R
+++ R/kNNSmoothing.R
@@ -1,13 +1,14 @@
 #' @title k-NN Smoothing of gene expression data
 #' @param log.filt.data filtered and normalised log-transformed genes x cells single-cell RNA-seq data matrix
 #' @param k number of nearest neighbours the data is smoothed over
-#' @return k-NN Smoothed data matrix
+#' @return k-NN smoothed data matrix
 #'
 #' @export
 #'
 
 kNNSmoothing <- function(log.filt.data, k = 11) {
 
+    # Reduce expression data to PC space for k-NN computation
     pca.data <-
         irlba::prcomp_irlba(
             x = t(as.matrix(log.filt.data)),
@@ -17,20 +18,40 @@ kNNSmoothing <- function(log.filt.data, k = 11) {
         )$x
     rownames(pca.data) <- colnames(log.filt.data)
 
+    # Determine k-nearest neighbours
     my.knn <-
-        nn2(
+        RANN::nn2(
             data = pca.data,
             k = k,
             searchtype = "priority",
             eps = 0
         )
+
+    # Extract indices of k-nearest neighbours
     nn.idx <- my.knn$nn.idx
+
+    # Discard self as nearest neighbour and set cell names
     nn.idx <- nn.idx[, -1]
     rownames(nn.idx) <- rownames(pca.data)
 
-    smooth.log.filt.data <- apply(nn.idx, 1, function(neighbours) {
-        Matrix::rowMeans(log.filt.data[, neighbours])
-    })
+    # Replace nearest neighbour indices with cell names
+    nn.mat <- t(apply(nn.idx, 1, function(neighbours){
+        rownames(nn.idx)[neighbours]
+    }))
+    colnames(nn.mat) <- paste0(seq(1,ncol(nn.mat)))
 
+    # Melt nearest neighbour matrix
+    nn.melt <- reshape2::melt(data = nn.mat)
+    nn.melt$Var2 <- NULL
+
+    # Construct adjacency matrix
+    adj.mat <- igraph::get.adjacency(graph = igraph::graph.edgelist(as.matrix(nn.melt), directed = TRUE))
+
+    # Compute smoothed data based on adjacency matrix
+    smooth.log.filt.data <- t(tcrossprod(x = adj.mat, y = log.filt.data)/(k-1))
+
+    print("kNN Smoothing - Done")
+
+    # Return smoothed data
     return(smooth.log.filt.data)
 }
