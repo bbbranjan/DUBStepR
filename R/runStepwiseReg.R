f4772dd9709ac324f33f8b5bde24ec55bd120d37
Bobby Ranjan <bbbranjan@gmail.com>
2020-02-25 17:43:53 +0800

    Added error to kNN computation and fixed bug.

--- R/runStepwiseReg.R
+++ R/runStepwiseReg.R
@@ -3,12 +3,12 @@
 #' @param filt.data filtered and normalised log-transformed genes x cells single-cell RNA-seq data matrix
 #' @param k number of nearest neighbours for CI computation
 #' @param num.pcs number of principal components to represent sc data. Default is 15.
-#' @param eps Acceptable error margin for kNN computation. Default is 0.
+#' @param error Acceptable error margin for kNN computation. Default is 0.
 #' @return optimal feature set
 #'
 #' @export
 #'
-runStepwiseReg <- function(ggc, filt.data, k = 10, num.pcs = 15, eps = 0) {
+runStepwiseReg <- function(ggc, filt.data, k = 10, num.pcs = 15, error = 0) {
 
     # Initialize variables
     ggc <- as(ggc, "dgCMatrix")
@@ -156,17 +156,20 @@ runStepwiseReg <- function(ggc, filt.data, k = 10, num.pcs = 15, eps = 0) {
             log.feature.data <-
                 filt.data[neighbour_feature_genes,]
             pca.obj <- irlba::prcomp_irlba(x = Matrix::t(log.feature.data), n = min(num.pcs, (length(neighbour_feature_genes) - 1)), center = TRUE, scale. = FALSE)
+
+            # spca.obj <- sparsepca::rspca(X = Matrix::t(log.feature.data), k = min(num.pcs, (length(neighbour_feature_genes) - 1)), center = TRUE, scale = FALSE)
+
             pca.data <- pca.obj$x
             rownames(pca.data) <- colnames(log.feature.data)
 
             # Compute k-NN distance
             system.time(
-                my.knn.2 <- RANN::nn2(
+                my.knn <- RANN::nn2(
                     data = pca.data,
                     k = (k+1),
                     treetype = "kd",
                     searchtype = "standard",
-                    eps = eps
+                    eps = error
                 )
             )
 
