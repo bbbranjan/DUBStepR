0b095a476290c326081b01eb189701da307a06f7
Kunal Mishra <kunal006@e.ntu.edu.sg>
2021-02-22 09:29:50 +0000

    Updated runStepwiseReg and added progress messages

--- R/logNormalize.R
+++ R/logNormalize.R
@@ -8,21 +8,21 @@ logNormalize <- function(raw.data, scale.factor = 10000) {
 
     # Compute sequencing depth vector
     seqDepthVec <- Matrix::colSums(raw.data)
-
+    
     # Scale sequencing depth by scaling factor
     seqDepthVec <- seqDepthVec/scale.factor
-
+    
     # Normalise data by cell
     norm.data <- sapply(seq_along(seqDepthVec), function(index) {
         raw.data[,index]/seqDepthVec[index]
     })
     colnames(norm.data) <- colnames(raw.data)
-
+    
     # Log-transform normalized data
     log.norm.data <- log(1+norm.data)
-
-    print("Log-normalization - Done")
-
+    
+    message("Log-normalization ✓")
+    
     # Return
     return(log.norm.data)
 }
